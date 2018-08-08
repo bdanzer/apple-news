c903b7708841a73f9a6b87470a245130bf924931
bdanzer <>
2018-08-08 11:49:57 -0500

    Changed function name to add_image_sizes to be more descriptive

--- admin/class-admin-apple-news.php
+++ admin/class-admin-apple-news.php
@@ -38,7 +38,7 @@ class Admin_Apple_News extends Apple_News {
 	public function __construct() {
 		// Register hooks.
 		add_action( 'admin_print_styles-toplevel_page_apple_news_index', array( $this, 'plugin_styles' ) );
-		add_action( 'init', array( $this, 'action_init' ) );
+		add_action( 'init', array( $this, 'add_image_sizes' ) );
 
 		/**
 		 * Admin_Settings builds the settings page for the plugin. Besides setting
@@ -216,8 +216,7 @@ class Admin_Apple_News extends Apple_News {
 	 *
 	 * @access public
 	 */
-	public function action_init() {
-
+	public function add_image_sizes() {
 		// Register custom image crops.
 		if ( 'yes' === self::$settings->enable_cover_art ) {
 			$image_sizes = self::get_image_sizes();
